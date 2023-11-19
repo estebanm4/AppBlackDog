bd927e10c8d97e37395d8673b353355b5a83c71a
yulian-z9 <yulian.santamaria.s@gmail.com>
2023-11-19 18:15:04 -0500

    UI de modulo login

---
+++ app/src/main/java/com/dadm/appblackdog/views/PasswordField.kt
@@ -0,0 +1,72 @@
+package com.dadm.appblackdog.views
+
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Key
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import com.dadm.appblackdog.R
+
+@Composable
+fun PasswordField(
+    value: String,
+    modifier: Modifier = Modifier,
+    onChange: (String)-> Unit,
+    submit: ()-> Unit,
+    label: String = stringResource(id = R.string.password_label),
+    placeholder: String = stringResource(id = R.string.password_placeholder)
+) {
+    var isPasswordVisible by remember { mutableStateOf(false) }
+    // icons
+
+    val leadingIcon = @Composable {
+        Icon(
+            Icons.Filled.Key,
+            contentDescription = "",
+            tint = MaterialTheme.colorScheme.primary
+        )
+    }
+    val trailingIcon = @Composable {
+        IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+            Icon(
+                if (isPasswordVisible) Icons.Filled.VisibilityOff else Icons.Filled.Visibility,
+                contentDescription = "",
+                tint = MaterialTheme.colorScheme.primary
+            )
+        }
+    }
+    // UI
+    TextField(
+        value = value,
+        onValueChange = onChange,
+        modifier = modifier,
+        leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password,
+        ),
+        keyboardActions = KeyboardActions(onDone = { submit() }),
+        placeholder = { Text(text = placeholder) },
+        label = { Text(text = label) },
+        singleLine = true,
+        visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
+    )
+}
